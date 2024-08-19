d00f94a42a4951a1d28eb7bd150aea0b439b5247
eeeseohyun <eeeseohyun@gmail.com>
2024-08-20 00:29:43 +0900

    ssh

--- src/main/java/com/example/tomyongji/auth/config/SecurityConfig.java
+++ src/main/java/com/example/tomyongji/auth/config/SecurityConfig.java
@@ -50,7 +50,6 @@ public class SecurityConfig {
                         // 이 밖에 모든 요청에 대해서 인증을 필요로 한다는 설정
 //                        .anyRequest().authenticated())
                         .anyRequest().permitAll())
-                .requiresChannel(channel -> channel.anyRequest().requiresSecure())
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
                         UsernamePasswordAuthenticationFilter.class

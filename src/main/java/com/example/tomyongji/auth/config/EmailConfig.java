099f9061e40ee268d1cf830a2ccb3c2b0acd4f40
eeeseohyun <eeeseohyun@gmail.com>
2024-09-26 15:20:44 +0900

    gmail create

--- src/main/java/com/example/tomyongji/auth/config/EmailConfig.java
+++ src/main/java/com/example/tomyongji/auth/config/EmailConfig.java
@@ -15,8 +15,8 @@ public class EmailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("eeeseohyun@gmail.com");
-        mailSender.setPassword("qxbz ntdu onlz airj");
+        mailSender.setUsername("tomyongji2024@gmail.com");
+        mailSender.setPassword("vggv rtim jonp ihby");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");

6caad6d2d7f9363281f3208676fe67db2638e935
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2024-12-24 01:11:35 +0900

    add balance logic

--- src/main/java/com/example/tomyongji/validation/ErrorMsg.java
+++ src/main/java/com/example/tomyongji/validation/ErrorMsg.java
@@ -12,8 +12,4 @@ public class ErrorMsg {
     public static final String EMPTY_CONTENT = "학생회비 사용 내용을 작성해주세요.";
     public static final String EMPTY_MONEY = "입출금 내역을 작성해주세요.";
     public static final String DUPLICATED_FLOW = "입금과 출금 둘 중 하나만 적어주세요.";
-
-
-
-
 }

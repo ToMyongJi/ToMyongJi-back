2ad2059fa999336ea37ecf4c93a969a8b92f9dcd
eeeseohyun <eeeseohyun@gmail.com>
2025-01-08 15:34:29 +0900

    Test:User 통합테스트
     Email 통합테스트 마무리
    
    Resolves #100

--- src/main/java/com/example/tomyongji/auth/dto/EmailDto.java
+++ src/main/java/com/example/tomyongji/auth/dto/EmailDto.java
@@ -1,12 +1,14 @@
 package com.example.tomyongji.auth.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class EmailDto {
     private String email;
 }

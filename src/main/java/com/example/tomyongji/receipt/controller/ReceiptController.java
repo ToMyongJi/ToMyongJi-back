f65e2e0d48a6b67aed24b1e83ab23ce31031fa89
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-03-24 00:40:55 +0900

    Refactor : updateReceipt() 매개변수 수정
    ReceiptController의 updateReceipt() 메서드도 clubCheck을 할 수 있도록 UserDetails 타입의 currentUser를 매개변수로 넣음
    
    Resolves: #157

--- src/main/java/com/example/tomyongji/receipt/controller/ReceiptController.java
+++ src/main/java/com/example/tomyongji/receipt/controller/ReceiptController.java
@@ -29,6 +29,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -88,9 +89,9 @@ public class ReceiptController {
     }
 
     @Operation(summary = "영수증 수정 api", description = "영수증 아이디를 통해 특정 영수증을 수정합니다.")
-    @PatchMapping //특정 영수증 수정
-    public ApiResponse<ReceiptDto> updateReceipt(@RequestBody ReceiptDto receiptDto) {
-        ReceiptDto updatedReceipt = receiptService.updateReceipt(receiptDto);
+    @PutMapping //특정 영수증 수정
+    public ApiResponse<ReceiptDto> updateReceipt(@RequestBody ReceiptDto receiptDto, @AuthenticationPrincipal UserDetails currentUser) {
+        ReceiptDto updatedReceipt = receiptService.updateReceipt(receiptDto, currentUser);
         return new ApiResponse<>(200, "영수증을 성공적으로 수정했습니다.", updatedReceipt);
     }
 }

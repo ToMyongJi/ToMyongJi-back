5dbfce39f9c650c574bf12b66631a7e89392e0a5
woojoo48 <wjddnwn0408@gmail.com>
2025-07-15 09:44:27 +0900

    Chore : StudentClubRepository 메서드 추가
    학생회의 verification을 전환하는 메서드 추가
    Update 작업으로 인한 쿼리문 추가
    
    Resolves: #178

--- src/main/java/com/example/tomyongji/receipt/repository/StudentClubRepository.java
+++ src/main/java/com/example/tomyongji/receipt/repository/StudentClubRepository.java
@@ -4,6 +4,7 @@ import com.example.tomyongji.admin.entity.President;
 import com.example.tomyongji.auth.entity.User;
 import com.example.tomyongji.receipt.entity.StudentClub;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -23,5 +24,7 @@ public interface StudentClubRepository extends JpaRepository<StudentClub, Long> 
 
     StudentClub findByStudentClubName(String clubName);
 
-
+    @Modifying
+    @Query("UPDATE StudentClub s SET s.verification = true WHERE s.id = :clubId")
+    void updateVerificationById(@Param("clubId") Long clubId);
 }

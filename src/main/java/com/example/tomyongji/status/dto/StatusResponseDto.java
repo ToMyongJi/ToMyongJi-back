375f17148dbb9ae4d7126f1738368c177ae9a678
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-10-09 22:51:08 +0900

    refactor: 정기점검 반환 DTO에 생성자 관련 어노테이션 및 저장시 직렬화 추가

--- src/main/java/com/example/tomyongji/status/dto/StatusResponseDto.java
+++ src/main/java/com/example/tomyongji/status/dto/StatusResponseDto.java
@@ -1,17 +1,26 @@
 package com.example.tomyongji.status.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class StatusResponseDto {
     private String status;
     private String message;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime startTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime expectedEndTime;
 
 }

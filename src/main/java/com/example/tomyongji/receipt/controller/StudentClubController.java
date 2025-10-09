5d909721ab38048534100d31d9cecdde5266c8aa
woojoo48 <wjddnwn0408@gmail.com>
2025-10-09 18:59:24 +0900

    [Feat] : 학생회 이전 api 컨트롤러 개발
    학생회 이전 api 컨트롤러 추가
    
    IssueNum : #262

--- src/main/java/com/example/tomyongji/receipt/controller/StudentClubController.java
+++ src/main/java/com/example/tomyongji/receipt/controller/StudentClubController.java
@@ -1,13 +1,19 @@
 package com.example.tomyongji.receipt.controller;
 
 import com.example.tomyongji.admin.dto.ApiResponse;
+import com.example.tomyongji.admin.dto.PresidentDto;
 import com.example.tomyongji.receipt.dto.ClubDto;
+import com.example.tomyongji.receipt.dto.TransferDto;
+import com.example.tomyongji.receipt.dto.TransferRequestDto;
 import com.example.tomyongji.receipt.service.CollegeService;
 import com.example.tomyongji.receipt.service.StudentClubService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,5 +39,18 @@ public class StudentClubController {
         @PathVariable("collegeId") Long collegeId) {
         List<ClubDto> clubs = studentClubService.getStudentClubById(collegeId);
         return new ApiResponse<>(200, "해당 단과대의 학생회를 성공적으로 조회했습니다.", clubs); // 200 OK
+    }
+
+    @Operation(summary = "학생회 이월/이전 api", description = "학생회 정보를 이월 합니다.")
+    @PostMapping("api/club/transfer")
+    public ApiResponse<TransferDto> transferStudentClub(
+        @RequestBody TransferRequestDto request,
+        @AuthenticationPrincipal UserDetails currentUser
+    ) {
+        TransferDto result = studentClubService.transferStudentClub(
+            request,
+            currentUser
+        );
+        return new ApiResponse<>(200, "학생회 이월이 성공적으로 완료되었습니다.", result);
     }
 }

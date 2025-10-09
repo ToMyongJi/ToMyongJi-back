3d135c8f8dd7e5ff59b31f37d3bbc9209cf8e5c7
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-10-09 22:50:25 +0900

    refactor: 정기점검 변경 DTO에 생성자 관련 어노테이션 추가

--- src/main/java/com/example/tomyongji/status/dto/MaintenanceUpdateRequestDto.java
+++ src/main/java/com/example/tomyongji/status/dto/MaintenanceUpdateRequestDto.java
@@ -1,13 +1,24 @@
 package com.example.tomyongji.status.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class MaintenanceUpdateRequestDto {
     private String status;
     private String message;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime startTime;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
     private LocalDateTime expectedEndTime;
 
 }

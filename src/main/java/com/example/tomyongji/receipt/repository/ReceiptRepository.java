1f6559acc8820fab067898302c1bc9521a76ff8c
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2025-12-17 15:55:36 +0900

    feat: allow public access to receipt paging by club ID

--- src/main/java/com/example/tomyongji/receipt/repository/ReceiptRepository.java
+++ src/main/java/com/example/tomyongji/receipt/repository/ReceiptRepository.java
@@ -5,6 +5,8 @@ import com.example.tomyongji.receipt.entity.StudentClub;
 
 import java.util.Date;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +14,8 @@ import org.springframework.data.repository.query.Param;
 public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
     List<Receipt> findAllByStudentClub(StudentClub studentClub);
     List<Receipt> findAllByStudentClubOrderByIdDesc(StudentClub studentClub);
+
+    Page<Receipt> findByStudentClub(StudentClub studentClub, Pageable pageable);
     boolean existsByDateAndContent(Date date, String content);
     void deleteAllByStudentClub(StudentClub studentClub);
     int countByStudentClub(StudentClub studentClub);

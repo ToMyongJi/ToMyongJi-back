6047bbbf64020ead4959a9b702f9f6831658b346
david3478 <inwookim03@naver.com>
2026-02-02 16:22:43 +0900

    feat: QnA 게시판 repository 생성
    QnaQuestionRepository -> 현재는 QnaQuestion에서 영속성 전이를 통해 답변을 관리하므로 사용되지
    않으나 조회 기능 개발 시 필요할 것 같아 남겨놓음

---
+++ src/main/java/com/example/tomyongji/qna/repository/QnaAnswerRepository.java
@@ -0,0 +1,8 @@
+package com.example.tomyongji.qna.repository;
+
+import com.example.tomyongji.qna.entity.QnaAnswer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QnaAnswerRepository extends JpaRepository<QnaAnswer,Long> {
+
+}
---
+++ src/main/java/com/example/tomyongji/qna/repository/QnaQuestionRepository.java
@@ -0,0 +1,9 @@
+package com.example.tomyongji.qna.repository;
+
+import com.example.tomyongji.qna.entity.QnaQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QnaQuestionRepository extends JpaRepository<QnaQuestion,Long> {
+
+
+}

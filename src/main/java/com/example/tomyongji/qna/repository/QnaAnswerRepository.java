14a0714bef7d2440e7f69ae87611ee9a79ccf8af
david3478 <inwookim03@naver.com>
2026-02-15 19:16:26 +0900

    refactor: Repository에 @EntityGraph 추가하여 연관 엔티티 최적화 조회
    IssueNum : #343

--- src/main/java/com/example/tomyongji/qna/repository/QnaAnswerRepository.java
+++ src/main/java/com/example/tomyongji/qna/repository/QnaAnswerRepository.java
@@ -3,11 +3,13 @@ package com.example.tomyongji.qna.repository;
 import com.example.tomyongji.qna.entity.QnaAnswer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface QnaAnswerRepository extends JpaRepository<QnaAnswer,Long> {
 
+    @EntityGraph(attributePaths = {"writer"})
     Page<QnaAnswer> findByQuestionId(Long questionId, Pageable pageable);
 }

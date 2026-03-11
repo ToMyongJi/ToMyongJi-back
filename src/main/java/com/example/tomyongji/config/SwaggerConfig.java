78a47ccc27f93255dbe114a93e13329536c9c145
woojoo48 <wjddnwn0408@gmail.com>
2026-03-11 13:28:36 +0900

    [Refactor] : Swagger JWT 입력 추가
    Swagger JWT 입력 추가
    
    IssueNum : #362

--- src/main/java/com/example/tomyongji/config/SwaggerConfig.java
+++ src/main/java/com/example/tomyongji/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.tomyongji.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String BEARER_AUTH = "BearerAuth";
+
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
@@ -20,10 +24,20 @@ public class SwaggerConfig {
             .version("v1.0.0")
             .description("투명지 API 입니다");
 
+        SecurityScheme bearerAuth = new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .in(SecurityScheme.In.HEADER)
+            .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+            .addList(BEARER_AUTH);
+
         return new OpenAPI()
-            .components(new Components())
-            // 여기서 servers에 "/"를 넣어주면 Swagger UI가
-            // https://<호스트>/swagger-ui 로부터 상대경로로 API 요청을 보냅니다.
+            .components(new Components()
+                .addSecuritySchemes(BEARER_AUTH, bearerAuth))
+            .addSecurityItem(securityRequirement)
             .addServersItem(new Server().url("/"))
             .info(info);
     }
